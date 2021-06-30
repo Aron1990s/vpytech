60eda08bddede73beedb969c8c9108d08a0287bc
zch1990s <zch1990s@gmail.com>
2021-06-30 17:05:57 +0800

    ok

---
+++ src/main/java/com/aron/vpythontech/controller/tech/TechController.java
@@ -0,0 +1,17 @@
+package com.aron.vpythontech.controller.tech;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @ClassName TechController
+ * @Description TODO
+ * @Author aron
+ * @Date 2021/6/30 15:53
+ **/
+public class TechController {
+
+    protected final static Logger logger = LoggerFactory.getLogger(TechController.class);
+
+
+}
