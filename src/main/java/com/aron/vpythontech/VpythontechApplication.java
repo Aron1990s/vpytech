45fc35f3e8822b53c93d20e7b2c8459b09abe0e4
zch1990s <zch1990s@gmail.com>
2021-07-04 15:34:50 +0800

    ok

--- src/main/java/com/aron/vpythontech/VpythontechApplication.java
+++ src/main/java/com/aron/vpythontech/VpythontechApplication.java
@@ -1,13 +1,19 @@
 package com.aron.vpythontech;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VpythontechApplication {
 
+    protected final static Logger logger = LoggerFactory.getLogger(VpythontechApplication.class);
+
+
     public static void main(String[] args) {
         SpringApplication.run(VpythontechApplication.class, args);
+        logger.info("[~~~乾杯~~梦想已在路上~~ (^_^)／★＼(^_^) ~~道路已在脚下~~乾杯~~~]");
     }
 
 }
